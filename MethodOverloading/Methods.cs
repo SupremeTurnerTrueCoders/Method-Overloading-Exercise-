39af440a82031b91bfb15ef0b5629f384aa04d2c
SupremeTurnerTrueCoders <email@example.com>
2024-04-17 17:30:32 -0400

    Completed overloading

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,11 @@
+﻿namespace MethodOverloading
+{
+
+
+var answer = methods.Add(2,2);
+
+Console.WriteLine(answer)
+
+
+}
+
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int add(int a, int b)
         {
-            Console.WriteLine("Hello, World!");
+            return a + b;
+        }
+        public static Decimal Add(Decimal a, Decimal b)
+        {
+
+
+
+            return a + b;
+
+
+
+        }
+        public static string Add(int a, int b, bool withDollars)
+        {
+            if (withDollars && (a + b) != 1)
+            {
+                return $"{a + b} dollars";
+            }
+            else if (withDollars && (a + b) == 1)
+            {
+                return $"{a + b} dollars";
+            }
+            else
+            {
+                return $"{a + b}";
+            }
+
         }
     }
 }
